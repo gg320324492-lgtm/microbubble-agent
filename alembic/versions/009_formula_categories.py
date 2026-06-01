6c4aa5c8a456d12fc3a63a553304fb1ff7dc57db
gg320324492-lgtm <gg320324492@gmail.com>
2026-06-01 15:52:48 +0800

    fix(alembic): 修复 009 down_revision 链断（'008' → '008_fix_embedding'）

--- alembic/versions/009_formula_categories.py
+++ alembic/versions/009_formula_categories.py
@@ -16,7 +16,7 @@ import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 
 revision: str = '009'
-down_revision: Union[str, None] = '008'
+down_revision: Union[str, None] = '008_fix_embedding'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
